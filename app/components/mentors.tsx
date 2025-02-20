2707b0ffb33026b5d56fa31cd53296bbfa16e7d7
Mansi Jamwal <jamwalmansi16@gmail.com>
2025-02-20 12:04:21 +0530

    mentor linkedin mobile layout fixed

--- app/components/mentors.tsx
+++ app/components/mentors.tsx
@@ -9,7 +9,7 @@ export default function Mentors() {
       <div className="text-xl pt-3 font-medium text-center">Experts Who Guide Your Learning</div>
       
       <div className="px-10 flex flex-wrap justify-center md:justify-between">
-      <div className="flex flex-col items-center mt-16 mx-10 bg-white p-6 rounded-2xl shadow-lg transition-transform hover:scale-105">
+  <div className="w-[400px] flex flex-col items-center mt-16  bg-white p-6 rounded-2xl shadow-lg transition-transform hover:scale-105">
   <div className="relative w-40 h-40 rounded-full overflow-hidden border-4 border-gray-300">
     <Image
       src="/static/shubham.jpeg"
@@ -20,17 +20,17 @@ export default function Mentors() {
     />
   </div>
   <div className="text-center mt-4 text-2xl font-bold text-gray-800">Shubham Bharti</div>
-  <div className="text-center mt-1 text-lg font-medium text-gray-600">
-    Computer Science Alumni, IIT BHU
+    <div className="text-center mt-1 text-lg font-medium text-gray-600">
+      Computer Science Alumni, IIT BHU
+    </div>
+    <Link
+      href="https://www.linkedin.com/in/shubham-bharti-64aa55b6/"
+      target="_blank"
+      className="mt-3 px-4 py-2 text-center bg-[#facc15] rounded-md text-sm font-semibold shadow-md hover:bg-yellow-500 transition"
+    >
+      Connect on LinkedIn
+    </Link>
   </div>
-  <Link
-    href="https://www.linkedin.com/in/shubham-bharti-64aa55b6/"
-    target="_blank"
-    className="mt-3 px-4 py-2 bg-[#facc15] rounded-lg text-sm font-semibold shadow-md hover:bg-yellow-500 transition"
-  >
-    Connect on LinkedIn
-  </Link>
-</div>
 
 
         <div className="flex flex-col items-center m-4">
